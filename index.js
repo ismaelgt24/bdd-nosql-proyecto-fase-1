87f9a65f4322a743373f9739aaa6042362ea6640
Ismael Guerrero <ismael@ismaelPC>
2024-07-18 15:43:44 -0400

    Uso de la clase GameAPI para lectura de los Datos [Correcion]

--- index.js
+++ index.js
@@ -14,190 +14,6 @@ const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
 
 const TOTAL_PAGES = 1;//Cantidad de paginas a leer en la API
 
-const getVideojuegos = async () => {
-
-    let fetchedGames = [];
-    let page = 1;
-    let theresNext = true;
-    // let gamesCount = 0;
-
-    try {
-        while (page<=TOTAL_PAGES && theresNext){
-
-            //Hacemos la solicitud a la API y almacenamos la respuesta:
-            const res = await axios.get(`${BASE_URL}games`, {
-            params: {//Estos son los parametros para la solicitud HTTP a la API
-                key: API_KEY,
-                page_size: PAGE_SIZE,//¿Por que retorna de 40 en 40 y no de 150 en 150?
-                page: page//Pagina de la API que se esta consumiendo
-            }
-            });
-            
-            const fetchedPage = res.data.results;
-            // gamesCount = res.data.count;
-
-            //Agregamos la pagina captada al array de juegos:
-            fetchedGames = fetchedGames.concat(res.data.results);//El atributo results es el arreglo de videojuegos en la respuesta http (Segun la documentacion de RAWG.IO)
-
-            // console.log(`En la pagina ${page} hemos captados ${fetchedPage.length} videojuegos`);
-
-            //Vamos a la siguiente pagina
-            page++;
-            theresNext = res.data.next;
-        }
-
-        // console.log(`Hemos captado un total de ${fetchedGames.length} juegos`);
-        
-        // console.log(fetchedGames[0]); 
-
-        //Finalmente retornamos el array de videojuegos captados:
-        return fetchedGames;
-    } catch (error) {
-        console.error('\n\nHubo un error solicitando los videojuegos: ', error);
-    }
-};
-
-
-const getVideojuegosByEmpresa = async (DevId) => {
-    // console.log(`buscando videojuegos de ${DevId}`);
-    let fetchedGames = [];
-    // let page = 1;
-    // let theresNext = true;
-    // let gamesCount = 0;
-
-    try {
-        // while (page<=TOTAL_PAGES && theresNext){
-
-            //Hacemos la solicitud a la API y almacenamos la respuesta:
-            const res = await axios.get(`${BASE_URL}games`, {
-            params: {//Estos son los parametros para la solicitud HTTP a la API
-                key: API_KEY,
-                page_size: PAGE_SIZE,//¿Por que retorna de 40 en 40 y no de 150 en 150?
-                // page: page,//Pagina de la API que se esta consumiendo
-                developers : DevId
-            }
-            });
-            
-            const fetchedPage = res.data.results;
-            // gamesCount = res.data.count;
-
-            if (!fetchedPage || fetchedPage.length === 0) {
-                console.warn(`No se encontraron videojuegos para el desarrollador con ID: ${DevId}`);
-                return fetchedGames;
-            }
-            
-            fetchedGames = fetchedGames.concat(res.data.results);//El atributo results es el arreglo de videojuegos en la respuesta http (Segun la documentacion de RAWG.IO)
-
-            // Agregar el campo DeveloperId a cada juego
-            fetchedGames = fetchedGames.map(game => {
-                game.DeveloperId = DevId;
-                game.platforms = game.platforms.map( p => {return p.platform.id});//Si se quiere guardar la plataforma
-                return game;
-            });
-            // console.log(`En la pagina ${page} hemos captados ${fetchedPage.length} videojuegos`);
-
-            //Vamos a la siguiente pagina
-            // page++;
-            // theresNext = res.data.next;
-        // }
-
-        // console.log(`Hemos captado un total de ${fetchedGames.length} juegos`);
-        
-        // console.log(fetchedGames[0]); 
-        //Finalmente retornamos el array de videojuegos captados:
-        return fetchedGames;
-    } catch (error) {
-        console.error('Hubo un error solicitando los videojuegos: ', error.message);
-    }
-};
-
-
-const getPlataformas = async () => {
-    
-    //Leemos las platafromas disponibles desde la API:
-    console.log("Leyendo Plataformas disponibles.\n")
-    let fetchedPlatforms = [];
-    let page = 1;
-    let theresNext = true;
-
-    try {
-        // while (page<=TOTAL_PAGES && theresNext){
-
-            //Hacemos la solicitud a la API y almacenamos la respuesta:
-            const res = await axios.get(`${BASE_URL}platforms`, {
-            params: {//Estos son los parametros para la solicitud HTTP a la API
-                key: API_KEY,
-                page_size: PAGE_SIZE,//¿Por que retorna de 40 en 40 y no de 150 en 150?
-                page: page//Pagina de la API que se esta consumiendo
-            }
-            });
-
-            const fetchedPage = res.data.results;
-
-            //Agregamos la pagina captada al array de juegos:
-            fetchedPlatforms = fetchedPlatforms.concat(res.data.results);//El atributo results es el arreglo de videojuegos en la respuesta http (Segun la documentacion de RAWG.IO)
-
-            // console.log(`En la pagina ${page} hemos captados ${fetchedPage.length} videojuegos`);
-
-            //Vamos a la siguiente pagina
-        //     page++;
-        //     theresNext = res.data.next;
-        // }
-
-        // console.log(`Hemos captado un total de ${fetchedPlatforms.length} plataformas`);
-        // console.log(fetchedPlatforms[0]);
-
-        console.log(`Se han leido ${fetchedPlatforms.length} Plataformas.`)
-        //Retornamos el array de plataformas captadas:
-        return fetchedPlatforms;
-        
-    } catch (error) {
-        console.error('\n\nHubo un error solicitando las plataformas: ', error);
-    }
-};
-
-
-const getEmpresas = async () => {
-    //Asumimos que las empresas son quienes desarrollan los videojuegos
-    //Por lo que llenaremos la coleccion empresas con desarrolladores
-    console.log("Leyendo Empresas  disponibles.\n")
-
-    let fetchedInterprise = [];
-    let page = 1;
-    let theresNext = true;
-
-    try {
-        // while (page<=TOTAL_PAGES && theresNext){
-        //     theresNext = false;
-                
-            let resDevelopers = await axios.get(`${BASE_URL}developers`, {
-                params: {//Estos son los parametros para la solicitud HTTP a la API
-                    key: API_KEY,
-                    page_size: PAGE_SIZE,//¿Por que retorna de 40 en 40 y no de 150 en 150?
-                    page: page//Pagina de la API que se esta consumiendo
-                }
-            });
-            
-            if(resDevelopers.status>=200 && resDevelopers.status<=299){
-                fetchedInterprise = fetchedInterprise.concat(resDevelopers.data.results);    
-                // console.log(`En la pagina ${page} hemos captado ${resDevelopers.data.results.length} Desarrolladores`);
-                if (resDevelopers.data.next) theresNext = true;
-            }
-
-            //Vamos a la siguiente pagina
-        //     page++;
-        // }
-
-        console.log(`Se han leido ${fetchedInterprise.length} Empresas.`)
-        //Retornamos las empresas leidas:
-        return fetchedInterprise;
-
-    } catch (error) {
-        console.error('\n\nHubo un error solicitando las Empresas: ', error);
-    }
-
-};
-
 const MongoDBClient = require('./servicios/MongoDBClient');
 const GameAPI = require('./servicios/GameAPI');
 
@@ -242,7 +58,7 @@ const GameAPI = require('./servicios/GameAPI');
         
         //Para poder resolver las 3 promesas de forma concurrente:
         // const solictudes = await Promise.all([getVideojuegos(),getPlataformas(),getEmpresas()]);
-        const solictudes = await Promise.all([getPlataformas(),getEmpresas()]);
+        const solictudes = await Promise.all([gameAPI.getPlataformas(),gameAPI.getEmpresas()]);
         
         //Separamos los listados captados
         plataformas = solictudes[0];
@@ -257,10 +73,10 @@ const GameAPI = require('./servicios/GameAPI');
             try {
                 //Lectura de los juegos desde la API de Rawg:
                 console.log(`\tLeyendo videojuegos desarrollados por ${empresa.name}.`)
-                let juegosLeidos = await getVideojuegosByEmpresa(empresa.id);
+                let juegosLeidos = await gameAPI.getVideojuegosByEmpresa(empresa.id);
 
                 //Impresion de prueba:
-                console.log(`\tPrimer videojuego leido: ${juegosLeidos[0].platforms}`)
+                // console.log(`\tPrimer videojuego leido: ${juegosLeidos[0].platforms}`)
 
                 //Aviso de juegos leidos:
                 console.log(juegosLeidos.length ? `\tSe leyeron ${juegosLeidos.length} desarrollados por ${empresa.name}\n` : `No se encontraron juegos desarrollados por ${empresa.name}\n`);
@@ -290,83 +106,83 @@ const GameAPI = require('./servicios/GameAPI');
 
 
 
-//Inserto las videojuegos
-for (const juego of videojuegos) {
-    const nuevoVideojuego = new Videojuego({
-      id: juego.id,
-      name: juego.name,
-      slug: juego.slug,
-      released: juego.released,
-      rating: juego.rating,
-      rating_top: juego.rating_top,
-      ratings_count: juego.ratings_count,
-      reviews_text_count: juego.reviews_text_count,
-      added: juego.added,
-      metacritic: juego.metacritic,
-      playtime: juego.playtime,
-      suggestions_count: juego.suggestions_count,
-      updated: juego.update,
-      reviews_count: juego.reviews_count,
-      platforms: juego.platforms,
-      DeveloperId: juego.DevId,
-    });
-    for (const rat of juego.ratings){//
-        const aux = {
-            tittle: rat.title,
-            count: rat.count,
-            percent: rat.percent,
-        };
-        nuevoVideojuego.tags.push(aux);
-    } 
-    for (const plat of juego.parent_platforms){//
-        const aux = {
+    //Inserto las videojuegos
+    for (const juego of videojuegos) {
+        const nuevoVideojuego = new Videojuego({
+        id: juego.id,
+        name: juego.name,
+        slug: juego.slug,
+        released: juego.released,
+        rating: juego.rating,
+        rating_top: juego.rating_top,
+        ratings_count: juego.ratings_count,
+        reviews_text_count: juego.reviews_text_count,
+        added: juego.added,
+        metacritic: juego.metacritic,
+        playtime: juego.playtime,
+        suggestions_count: juego.suggestions_count,
+        updated: juego.update,
+        reviews_count: juego.reviews_count,
+        platforms: juego.platforms,
+        DeveloperID: juego.DeveloperID,
+        });
+        for (const rat of juego.ratings){//
+            const aux = {
+                tittle: rat.title,
+                count: rat.count,
+                percent: rat.percent,
+            };
+            nuevoVideojuego.ratings.push(aux);
+        } 
+        for (const plat of juego.ratings){//
+            const aux = {
+                id: plat.id,
+                name: plat.name,
+                slug: plat.slug,
+            };
+            nuevoVideojuego.ratings.push(aux);
+        } 
+        for (const gen of juego.genres){//
+            const aux = {
+                id: gen.id,
+                name: gen.name,
+                slug: gen.slug,
+            };
+            nuevoVideojuego.genres.push(aux);
+        } 
+        for (const tag of juego.tags){//
+            const aux = {
+                name: tag.name,
+                slug: tag.slug,
+            };
+            nuevoVideojuego.tags.push(aux);
+        }
+        await mongoClient.insertar('Videojuego', nuevoVideojuego);
+    }
+    //Inserto las plataformas
+        for (const plat of plataformas) {
+            const nuevaPlataforma = new Plataforma({
             id: plat.id,
             name: plat.name,
             slug: plat.slug,
-        };
-        nuevoVideojuego.tags.push(aux);
-    } 
-    for (const gen of juego.genres){//
-        const aux = {
-            id: gen.id,
-            name: gen.name,
-            slug: gen.slug,
-        };
-        nuevoVideojuego.tags.push(aux);
-    } 
-    for (const tag of juego.tags){//
-        const aux = {
-            name: tag.name,
-            slug: tag.slug,
-        };
-        nuevoVideojuego.tags.push(aux);
-    }
-    await mongoClient.insertar('Videojuego', nuevoVideojuego);
-}
-//Inserto las plataformas
-    for (const plat of plataformas) {
-        const nuevaPlataforma = new Plataforma({
-          id: plat.id,
-          name: plat.name,
-          slug: plat.slug,
-          games_count: plat.games_count
-        });
-        await mongoClient.insertar('Plataforma', nuevaPlataforma);
-    }
-//inserto las empresas
-    for (const empresa of empresas) {
-        const nuevaEmpresa = new Empresa({
-          id: empresa.id,
-          name: empresa.name,
-          slug: empresa.slug,
-          games_count: empresa.games_count,
-          games: empresa.games.id // linea basura que coloque para mentirle al codigo diciendo que insertaba algo
-        });
-        for (const gam of empresa.games){//se insertan los ids de los juegos de cada empresa para poder hacer busquedas posteriores de juegos por empresa desarrolladora
-            nuevaEmpresa.games.push(gam.id);
-        }  
-        await mongoClient.insertar('Empresa', nuevaEmpresa);
-    }
+            games_count: plat.games_count
+            });
+            await mongoClient.insertar('Plataforma', nuevaPlataforma);
+        }
+    //inserto las empresas
+        for (const empresa of empresas) {
+            const nuevaEmpresa = new Empresa({
+            id: empresa.id,
+            name: empresa.name,
+            slug: empresa.slug,
+            games_count: empresa.games_count,
+            games: empresa.games.id // linea basura que coloque para mentirle al codigo diciendo que insertaba algo
+            });
+            for (const gam of empresa.games){//se insertan los ids de los juegos de cada empresa para poder hacer busquedas posteriores de juegos por empresa desarrolladora
+                nuevaEmpresa.games.push(gam.id);
+            }  
+            await mongoClient.insertar('Empresa', nuevaEmpresa);
+        }
 
     await mongoClient.close();
 })();
--- servicios/GameAPI.js
+++ servicios/GameAPI.js
@@ -12,7 +12,7 @@ class GameAPI {
         let allGames = [];
         let currentPage = 1;
 
-        const response = await axios.get(`https://www.giantbomb.com/api/platforms/?api_key=67439c253b917bd65f84366c1c5f1c75292b7066&filter=install_base:gt:1000`, {
+        const response = await axios.get(`https://www.giantbomb.com/api/platforms/?this.apiKey=67439c253b917bd65f84366c1c5f1c75292b7066&filter=install_base:gt:1000`, {
             params: {
                 format: 'json' // Request JSON response format
               }
@@ -20,6 +20,151 @@ class GameAPI {
         console.log(response.data.results)
         
     }
+
+    async getVideojuegosByEmpresa(DevId) {
+        // console.log(`buscando videojuegos de ${DevId}`);
+        let fetchedGames = [];
+    
+        try {
+    
+            //Hacemos la solicitud a la API y almacenamos la respuesta:
+            const res = await axios.get(`${this.baseUrl}games`, {
+            params: {//Estos son los parametros para la solicitud HTTP a la API
+                key: this.apiKey,
+                page_size: this.pageSize,//¿Por que retorna de 40 en 40 y no de 150 en 150?
+                // page: page,//Pagina de la API que se esta consumiendo
+                developers : DevId
+            }
+            });
+                
+            const fetchedPage = res.data.results;
+            // gamesCount = res.data.count;
+
+            if (!fetchedPage || fetchedPage.length === 0) {
+                console.warn(`No se encontraron videojuegos para el desarrollador con ID: ${DevId}`);
+                return fetchedGames;
+            }
+            
+            fetchedGames = fetchedGames.concat(res.data.results);//El atributo results es el arreglo de videojuegos en la respuesta http (Segun la documentacion de RAWG.IO)
+
+            // Agregar el campo DeveloperId a cada juego
+            fetchedGames = fetchedGames.map(game => {
+                game.DeveloperID = DevId;
+                game.platforms = game.platforms.map( p => {return p.platform.id});//Si se quiere guardar la plataforma
+                return game;
+            });
+            
+            // console.log(fetchedGames[0]); 
+            //Finalmente retornamos el array de videojuegos captados:
+            return fetchedGames;
+        } catch (error) {
+            console.error('Hubo un error solicitando los videojuegos: ', error.message);
+        }
+    };
+
+    async getPlataformas(){
+    
+        //Leemos las platafromas disponibles desde la API:
+        console.log("Leyendo Plataformas disponibles.\n")
+        let fetchedPlatforms = [];
+    
+        try {
+
+            // console.log(`Solicitud a realizar: ${this.baseUrl}platforms`)
+            //Hacemos la solicitud a la API y almacenamos la respuesta:
+            const res = await axios.get(`${this.baseUrl}platforms`, {
+                params: {//Estos son los parametros para la solicitud HTTP a la API
+                    key: this.apiKey,
+                }
+            });
+
+            const fetchedPage = res.data.results;
+
+            //Agregamos la pagina captada al array de juegos:
+            fetchedPlatforms = fetchedPlatforms.concat(res.data.results);//El atributo results es el arreglo de videojuegos en la respuesta http (Segun la documentacion de RAWG.IO)
+    
+            console.log(`Se han recibido ${fetchedPlatforms.length} Plataformas.`)
+            //Retornamos el array de plataformas captadas:
+            return fetchedPlatforms;
+            
+        } catch (error) {
+            console.error('\n\nHubo un error solicitando las plataformas: ', error);
+        }
+    };
+
+    async getEmpresas() {
+        //Asumimos que las empresas son quienes desarrollan los videojuegos
+        //Por lo que llenaremos la coleccion empresas con desarrolladores
+        console.log("Leyendo Empresas  disponibles.\n")
+    
+        let fetchedInterprise = [];
+    
+        try {
+                //Hacemos la solicitud y la guardamos en un arreglo:
+                let resDevelopers = await axios.get(`${this.baseUrl}developers`, {
+                    params: {//Estos son los parametros para la solicitud HTTP a la API
+                        key: this.apiKey,
+                        page_size: this.pageSize,//¿Por que retorna de 40 en 40 y no de 150 en 150?
+                    }
+                });
+                
+                //Evaluamos el estado de la respuesta de la solicitud:
+                if(resDevelopers.status>=200 && resDevelopers.status<=299){
+                    fetchedInterprise = fetchedInterprise.concat(resDevelopers.data.results);    
+                    // console.log(`En la pagina ${page} hemos captado ${resDevelopers.data.results.length} Desarrolladores`);
+                }
+    
+            console.log(`Se han recibido ${fetchedInterprise.length} Empresas.`)
+            
+            //Retornamos las empresas leidas:
+            return fetchedInterprise;
+    
+        } catch (error) {
+            console.error('\n\nHubo un error solicitando las Empresas: ', error);
+        }
+    
+    };
+
+
+    async getVideojuegosByEmpresa(DevId) {
+        // console.log(`buscando videojuegos de ${DevId}`);
+        let fetchedGames = [];
+
+        try {
+
+                //Hacemos la solicitud a la API y almacenamos la respuesta:
+                const res = await axios.get(`${this.baseUrl}games`, {
+                params: {//Estos son los parametros para la solicitud HTTP a la API
+                    key: this.apiKey,
+                    page_size: this.pageSize,//¿Por que retorna de 40 en 40 y no de 150 en 150?
+                    developers : DevId
+                }
+                });
+                
+                const fetchedPage = res.data.results;
+
+                if (!fetchedPage || fetchedPage.length === 0) {
+                    console.warn(`No se encontraron videojuegos para el desarrollador con ID: ${DevId}`);
+                    return fetchedGames;
+                }
+                
+                fetchedGames = fetchedGames.concat(res.data.results);//El atributo results es el arreglo de videojuegos en la respuesta http (Segun la documentacion de RAWG.IO)
+
+                // Agregar el campo DeveloperId a cada juego
+                fetchedGames = fetchedGames.map(game => {
+                    game.DeveloperID = DevId;
+                    game.platforms = game.platforms.map( p => {return p.platform.id});//Si se quiere guardar la plataforma
+                    return game;
+                });
+
+            //Finalmente retornamos el array de videojuegos captados:
+            return fetchedGames;
+        } catch (error) {
+            console.error('Hubo un error solicitando los videojuegos: ', error.message);
+        }
+    };
+
+
 }
 
 module.exports = GameAPI;
